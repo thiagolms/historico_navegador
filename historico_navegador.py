9af03880977ee6798bb849f4b8ebebfe0b3fb1cf
thiagolms <thiagopinheiiro@live.com>
2023-11-14 21:05:54 -0300

    armazenando em pilha

--- historico_navegador.py
+++ historico_navegador.py
@@ -9,22 +9,34 @@ class HistoricoNavegador:
 
     def voltar_pagina(self):
         try:
+            pagina_atual = self.pilha_paginas.desempilhar()
+            pagina_anterior = self.pilha_paginas.topo()
+            print("Voltando para:", pagina_anterior)
+            self.pilha_paginas.empilhar(pagina_atual)
             return self.pilha_paginas.desempilhar()
         except IndexError:
             print("Não há páginas anteriores no histórico.")
 
     def avancar_pagina(self):
         if self.pilha_paginas.tamanho() > 1:
-            pagina_atual = self.pilha_paginas.desempilhar()
-            proxima_pagina = self.pilha_paginas.topo()
-            
+            pagina_atual = self.pilha_paginas.topo()
+            proxima_pagina = self.pilha_paginas.desempilhar()
             print("Avançando para:", proxima_pagina)
             self.pilha_paginas.empilhar(pagina_atual)
+            return proxima_pagina
         else:
             print("Não há páginas seguintes no histórico.")
+            return None
         pass
 
     def visualizar_historico(self):
         print("Histórico de Navegação:")
         for pagina in reversed(self.pilha_paginas.itens):
             print(pagina)
+
+    def pagina_atual(self):
+        try:
+            return self.pilha_paginas.topo()
+        except IndexError:
+            print("A pilha está vazia. Nenhuma página atual.")
+            return None
--- simulador_navegacao.py
+++ simulador_navegacao.py
@@ -5,12 +5,17 @@ historico = HistoricoNavegador()
 historico.adicionar_pagina("www.exemplo.com/pagina1")
 historico.adicionar_pagina("www.exemplo.com/pagina2")
 historico.adicionar_pagina("www.exemplo.com/pagina3")
+historico.adicionar_pagina("www.exemplo.com/pagina4")
+
 
 historico.visualizar_historico()
 
-pagina_anterior = historico.voltar_pagina()
-print("Voltando para:", pagina_anterior)
+print("Página atual:", historico.pagina_atual())
 
-pagina_posterior = historico.avancar_pagina()
+historico.voltar_pagina()
+print("Página após voltar:", historico.pagina_atual())
+
+historico.avancar_pagina()
+print("Página após avançar:", historico.pagina_atual())
 
 historico.visualizar_historico()
