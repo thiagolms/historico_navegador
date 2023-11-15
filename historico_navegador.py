51bdab6d51cc1f122f1400b3b95a5aaa089742bd
Daniel <danielcorreiaborges9@hotmail.com>
2023-11-14 21:50:58 -0300

    correções

--- historico_navegador.py
+++ historico_navegador.py
@@ -2,41 +2,42 @@ from pilha import Pilha
 
 class HistoricoNavegador:
     def __init__(self):
-        self.pilha_paginas = Pilha()
+        self.pilha_paginas_visitadas = Pilha()
+        self.pilha_paginas_futuras = Pilha()
 
     def adicionar_pagina(self, pagina):
-        self.pilha_paginas.empilhar(pagina)
+        self.pilha_paginas_visitadas.empilhar(pagina)
+        # Ao adicionar uma nova página, limpa a pilha de páginas futuras
+        self.pilha_paginas_futuras = Pilha()
 
     def voltar_pagina(self):
         try:
-            pagina_atual = self.pilha_paginas.desempilhar()
-            pagina_anterior = self.pilha_paginas.topo()
-            print("Voltando para:", pagina_anterior)
-            self.pilha_paginas.empilhar(pagina_atual)
-            return self.pilha_paginas.desempilhar()
+            pagina_atual = self.pilha_paginas_visitadas.desempilhar()
+            pagina_atualv = self.pilha_paginas_visitadas.topo()
+            print("Voltando para:", pagina_atualv)
+            self.pilha_paginas_futuras.empilhar(pagina_atual)
+            return pagina_atual
         except IndexError:
             print("Não há páginas anteriores no histórico.")
 
     def avancar_pagina(self):
-        if self.pilha_paginas.tamanho() > 1:
-            pagina_atual = self.pilha_paginas.topo()
-            proxima_pagina = self.pilha_paginas.desempilhar()
-            print("Avançando para:", proxima_pagina)
-            self.pilha_paginas.empilhar(pagina_atual)
-            return proxima_pagina
-        else:
+        try:
+            pagina_futura = self.pilha_paginas_futuras.desempilhar()
+            print("Avançando para:", pagina_futura)
+            self.pilha_paginas_visitadas.empilhar(pagina_futura)
+            return pagina_futura
+        except IndexError:
             print("Não há páginas seguintes no histórico.")
             return None
-        pass
 
     def visualizar_historico(self):
         print("Histórico de Navegação:")
-        for pagina in reversed(self.pilha_paginas.itens):
+        for pagina in reversed(self.pilha_paginas_visitadas.itens):
             print(pagina)
 
     def pagina_atual(self):
         try:
-            return self.pilha_paginas.topo()
+            return self.pilha_paginas_visitadas.topo()
         except IndexError:
             print("A pilha está vazia. Nenhuma página atual.")
             return None
